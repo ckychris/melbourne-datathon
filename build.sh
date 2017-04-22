d0e3ba9f459de92210aba42d548bf36b21c71442
Nathan Faggian <nathan.faggian@gmail.com>
2017-04-22 21:22:15 +1000

    Updates

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ROOT=`pwd`
 # Pull the data for the experiment
 
 pushd data
-wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
+wget -O gdrive "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
 chmod +x gdrive
 ./gdrive download 0B9t_F6MeU1IcTXZMMGpQNDFGQU0
 unzip -P J34#PP3_MelbDatathon2017 MelbDatathon2017.zip
@@ -20,7 +20,7 @@ $HOME/conda/bin/conda config --add channels conda-forge
 $HOME/conda/bin/conda update conda --yes
 export PATH=$PATH:$HOME/conda/bin
 
-# Switch to the setup directory, create the conda env and build the database
+# Switch to the setup directory, create the conda env and build the database.
 pushd setup
 conda env create -f environment.yml
 source activate kaggle
