08b34a662ca899ad964b097af0d7b1a27becb94d
Nathan Faggian <nathan.faggian@gmail.com>
2017-04-22 21:07:35 +1000

    Updates to build

--- build.sh
+++ build.sh
@@ -2,6 +2,19 @@
 
 ROOT=`pwd`
 
+pushd data
+
+wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
+chmod +x gdrive
+./gdrive 0B9t_F6MeU1IcTXZMMGpQNDFGQU0 download --access-token 4/x_nrJJGzsuK-xORW2MC3_9635pLVwyPJk8GjEizkY5w
+# Unzip the password protected zipfile
+unzip -P J34#PP3_MelbDatathon2017 MelbDatathon2017.zip
+mkdir raw
+mv MelbDatathon2017/* raw/
+rm -rf MelbDatathon2017*
+
+popd
+
 # Pull and install miniconda
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -19,17 +32,6 @@ conda env create -f environment.yml
 # Activate the python environment
 source activate kaggle
 
-# Build the database
 bash database.sh
-
-# Retrieve the dataset
-python retrieve.py
-
-popd
-
-pushd data
-
-# Unzip the password protected zipfile
-unzip -P J34#PP3_MelbDatathon2017 raw_data.zip
 
 popd
