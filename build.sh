e397f0dda09c4ccf48456179497a8aaf62180a7c
Nathan Faggian <nathan.faggian@gmail.com>
2017-04-22 21:15:11 +1000

    Updates

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ROOT=`pwd`
 pushd data
 wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
 chmod +x gdrive
-bash gdrive download 0B9t_F6MeU1IcTXZMMGpQNDFGQU0
+. gdrive download 0B9t_F6MeU1IcTXZMMGpQNDFGQU0
 # Unzip the password protected zipfile
 unzip -P J34#PP3_MelbDatathon2017 MelbDatathon2017.zip
 mkdir -p raw;
