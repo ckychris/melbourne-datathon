14b355632d1e54548ae4f5c09cf510115dcd4fb9
Nathan Faggian <nathan.faggian@gmail.com>
2017-04-22 21:21:12 +1000

    Update

--- build.sh
+++ build.sh
@@ -2,18 +2,14 @@
 
 ROOT=`pwd`
 
-# Pull the datasets
+# Pull the data for the experiment
 
 pushd data
 wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download
 chmod +x gdrive
 ./gdrive download 0B9t_F6MeU1IcTXZMMGpQNDFGQU0
-# Unzip the password protected zipfile
 unzip -P J34#PP3_MelbDatathon2017 MelbDatathon2017.zip
-mkdir -p raw
-mv MelbDatathon2017/* raw/
-rm -rf MelbDatathon2017*
-
+mv MelbDatathon2017 raw
 popd
 
 # Pull and install miniconda
@@ -24,15 +20,9 @@ $HOME/conda/bin/conda config --add channels conda-forge
 $HOME/conda/bin/conda update conda --yes
 export PATH=$PATH:$HOME/conda/bin
 
-# Switch to the setup directory
+# Switch to the setup directory, create the conda env and build the database
 pushd setup
-
-# Build and then activate the conda environment
 conda env create -f environment.yml
-
-# Activate the python environment
 source activate kaggle
-
 bash database.sh
-
 popd
